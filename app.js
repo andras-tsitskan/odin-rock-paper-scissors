128e3b5e8653047666efdc8991fd10d4087f1fe2
Andraš <andras.tsitskan@gmail.com>
2022-06-16 15:35:04 +0300

    Add playSingleRound()

--- app.js
+++ app.js
@@ -29,4 +29,38 @@ function getPlayerSelection() {
 
 getPlayerSelection();
 
-// function singleRound(playerSelection, computerSelection) {}
+function playSingleRound(playerSelection, computerSelection) {
+  let roundResult;
+  if (playerSelection === computerSelection) {
+    roundResult = "It is a tie!";
+  } else {
+    switch (true) {
+      case playerSelection === "rock" && computerSelection === "paper":
+        roundResult = "You lose. :( Paper beats rock.";
+        break;
+      case playerSelection === "rock" && computerSelection === "scissors":
+        roundResult = "You win! :) Rock beats scissors.";
+        break;
+      case playerSelection === "paper" && computerSelection === "rock":
+        roundResult = "You win! :) Paper beats rock.";
+        break;
+      case playerSelection === "paper" && computerSelection === "scissors":
+        roundResult = "You lose. :( Scissors beat paper.";
+        break;
+      case playerSelection === "scissors" && computerSelection === "rock":
+        roundResult = "You lose. :( Rock beats scissors.";
+        break;
+      case playerSelection === "scissors" && computerSelection === "paper":
+        roundResult = "You win! :) Scissors beat paper.";
+        break;
+
+      default:
+        roundResult = "Something went wrong. Try again.";
+        break;
+    }
+  }
+  return roundResult;
+}
+
+playSingleRound(getComputerSelection(), getPlayerSelection());
+console.log(roundResult);
