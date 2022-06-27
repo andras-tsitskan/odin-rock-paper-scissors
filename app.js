c6821f08e55da306a9dc0a550027db1f4ef33876
Andraš <andras.tsitskan@gmail.com>
2022-06-27 16:18:29 +0300

    Add scorekeeping

--- app.js
+++ app.js
@@ -35,6 +35,9 @@ function getPlayerSelection(event) {
   return event.target.textContent.toLowerCase();
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
 function playRound() {
   let playerSelection = getPlayerSelection(event);
   playerChoiceSpan.textContent = playerSelection;
@@ -51,58 +54,84 @@ function playRound() {
     case playerSelection === computerSelection:
       round.result = "tie";
       round.message = "It is a tie!";
+
       break;
     case playerSelection === "rock" && computerSelection === "paper":
       round.result = "loss";
       round.message = "You lose. :( Paper beats rock.";
+      computerScore++;
       break;
     case playerSelection === "rock" && computerSelection === "scissors":
       round.result = "victory";
       round.message = "You win! :) Rock beats scissors.";
+      playerScore++;
       break;
     case playerSelection === "paper" && computerSelection === "rock":
       round.result = "victory";
       round.message = "You win! :) Paper beats rock.";
+      playerScore++;
       break;
     case playerSelection === "paper" && computerSelection === "scissors":
       round.result = "loss";
       round.message = "You lose. :( Scissors beat paper.";
+      computerScore++;
       break;
     case playerSelection === "scissors" && computerSelection === "rock":
       round.result = "loss";
       round.message = "You lose. :( Rock beats scissors.";
+      computerScore++;
       break;
     case playerSelection === "scissors" && computerSelection === "paper":
       round.result = "victory";
       round.message = "You win! :) Scissors beat paper.";
+      playerScore++;
       break;
   }
 
   roundResultSpan.textContent = round.message;
+  playerScoreSpan.textContent = playerScore;
+  computerScoreSpan.textContent = computerScore;
   return round;
 }
 
 function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-  for (let roundNumber = 0; roundNumber < 5; roundNumber++) {
-    let roundResult = playRound(getPlayerSelection(), getComputerSelection());
-    if (roundResult.includes("tie")) {
-      continue;
-    } else {
-      roundResult.includes("win") ? playerScore++ : computerScore++;
+  while (playerScore < 5 || computerScore < 5) {
+    playRound();
+
+    let roundResult = playRound().result;
+
+    switch (roundResult) {
+      case "tie":
+        break;
+      case "victory":
+        playerScore++;
+        break;
+      case "loss":
+        computerScore++;
+        break;
     }
+
+    playerScoreSpan.textContent = playerScore;
+    computerScoreSpan.textContent = computerScore;
   }
-  console.log("Five rounds have finished.");
-  console.log(`Player score: ${playerScore}.`);
-  console.log(`Computer score: ${computerScore}.`);
-  if (playerScore > computerScore) {
-    console.log("You won the game! Hurray!");
-  } else if (playerScore < computerScore) {
-    console.log("You lost the game, sorry. :(");
-  } else {
-    console.log("The game ended in tie.");
-  }
+
+  // for (let i = 0; i < 5; i++) {
+  //   if (roundResult.includes("tie")) {
+  //     continue;
+  //   } else {
+  //     roundResult.includes("win") ? playerScore++ : computerScore++;
+  //   }
+  // }
+  // console.log("Five rounds have finished.");
+  // console.log(`Player score: ${playerScore}.`);
+  // console.log(`Computer score: ${computerScore}.`);
+  // if (playerScore > computerScore) {
+  //   console.log("You won the game! Hurray!");
+  // } else if (playerScore < computerScore) {
+  //   console.log("You lost the game, sorry. :(");
+  // } else {
+  //   console.log("The game ended in tie.");
+  // }
 }
 
 // game();
