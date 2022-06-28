9ce859fec9e93fc9e2c57c8d15397688096b9cd3
Andraš <andras.tsitskan@gmail.com>
2022-06-28 13:30:32 +0300

    Accepted changes upon merge

--- app.js
+++ app.js
@@ -89,91 +89,11 @@ function getComputerSelection() {
       break;
     case 3:
       computerSelection = "scissors";
-<<<<<<< HEAD
-      break;
-    default:
-=======
->>>>>>> rps-ui
       break;
   }
   return computerSelection;
 }
 
-<<<<<<< HEAD
-function getPlayerSelection() {
-  let playerSelection = prompt("Select rock, paper or scissors.").toLowerCase();
-  return playerSelection;
-}
-
-function playRound(playerSelection, computerSelection) {
-  const round = {
-    result: "",
-    message: "",
-  };
-
-  if (playerSelection === computerSelection) {
-    round.result = "tie";
-    round.message = "It is a tie!";
-  } else if (playerSelection === "rock" && computerSelection === "paper") {
-    round.result = "loss";
-    round.message = "You lose. :( Paper beats rock.";
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    round.result = "victory";
-    round.message = "You win! :) Rock beats scissors.";
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    round.result = "victory";
-    round.message = "You win! :) Paper beats rock.";
-  } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    round.result = "loss";
-    round.message = "You lose. :( Scissors beat paper.";
-  } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    round.result = "loss";
-    round.message = "You lose. :( Rock beats scissors.";
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    round.result = "victory";
-    round.message = "You win! :) Scissors beat paper.";
-  } else {
-    round.message = "Something went wrong. Try again.";
-  }
-
-  return round;
-}
-
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-
-  for (let i = 0; i < 5; i++) {
-    let round = playRound(getPlayerSelection(), getComputerSelection());
-    let roundResult = round.result;
-    let roundMessage = round.message;
-
-    if (roundResult === "tie") {
-      console.log(roundMessage);
-      continue;
-    } else if (roundResult === "victory") {
-      playerScore++;
-      console.log(roundMessage);
-    } else if (roundResult === "loss") {
-      computerScore++;
-      console.log(roundMessage);
-    } else {
-      console.log(roundMessage);
-      continue;
-    }
-  }
-
-  console.log("Five rounds have finished.");
-  console.log(`Player score: ${playerScore}.`);
-  console.log(`Computer score: ${computerScore}.`);
-
-  if (playerScore > computerScore) {
-    console.log("You won the game! Hurray!");
-  } else if (playerScore < computerScore) {
-    console.log("You lost the game, sorry. :(");
-  } else {
-    console.log("The game ended in tie.");
-=======
 function getPlayerSelection(event) {
   return event.target.textContent.toLowerCase();
 }
@@ -188,7 +108,6 @@ function gameOver() {
     messageSpan.classList.add("highlight");
     messageSpan.textContent =
       "That's bad, computer won the game. Click reset to try again.";
->>>>>>> rps-ui
   }
 }
 
